84c4a3647c1aacaa7a4744443edab4ef4142a663
Krunalgiri Goswami <krunalgiricg2004@gmail.com>
2024-06-10 11:34:19 +0530

    Added Math and Random Methods program

--- Math_and_Random/DiceRoll.java
+++ Math_and_Random/DiceRoll.java
@@ -1,2 +1,16 @@
-package Math_and_Random;public class DiceRoll {
+package Math_and_Random;
+
+public class DiceRoll {
+
+    int roll(){
+        double random = Math.random() * 6;
+        return (int) Math.ceil(random);
+    }
+
+    public static void main(String[] args) {
+        DiceRoll dr = new DiceRoll();
+        for (int i = 0; i <= 6; i++){
+            System.out.println(dr.roll());
+        }
+    }
 }
