d535deeef439766f50dc8ccb839aa71c978e0243
Krunalgiri Goswami <krunalgiricg2004@gmail.com>
2024-06-16 21:26:53 +0530

    Added 2DArray Programs

---
+++ Arrays/MaxElement2DArray.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class MaxElement2DArray {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int max = array[0][0];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] > max) {
+                    max = array[i][j];
+                }
+            }
+        }
+
+        System.out.println("The maximum element in the 2D array is: " + max);
+    }
+}
+
