beb6b564ff385c5e5dc3d5f2f19952d0bc965b16
Krunalgiri Goswami <krunalgiricg2004@gmail.com>
2024-06-18 21:26:37 +0530

    Added New Programs

---
+++ Decision_Making/IfDemo.java
@@ -0,0 +1,17 @@
+package Decision_Making;
+
+import java.util.*;
+
+class IfDemo {
+    public static void main(String args[])
+    {
+        int i = 10;
+
+        if (i < 15)
+            System.out.println("Inside If block");
+        System.out.println("10 is less than 15");
+
+        System.out.println("I am Not in if");
+    }
+}
+
---
+++ Decision_Making/IfElseDemo.java
@@ -0,0 +1,16 @@
+package Decision_Making;
+
+import java.util.*;
+
+class IfElseDemo {
+    public static void main(String args[])
+    {
+        int i = 10;
+
+        if (i < 15)
+            System.out.println("i is smaller than 15");
+        else
+            System.out.println("i is greater than 15");
+    }
+}
+
